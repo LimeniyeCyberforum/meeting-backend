e0732cd30b4394b05809fa103d71bfb782cdaa7e
limeniye <limeniye@gmail.com>
2022-06-01 04:22:46 +0300

    - Added Message;

---
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Model/Message.cs
@@ -0,0 +1,16 @@
+﻿namespace MeetingGrpc.Server.Model
+{
+    public class Message
+    {
+        public Guid Guid { get; }
+        public string Content { get; }
+        public User User { get; }
+
+        public Message(Guid guid, string content, User user)
+        {
+            Guid = guid;
+            Content = content;
+            User = user;
+        }
+    }
+}
