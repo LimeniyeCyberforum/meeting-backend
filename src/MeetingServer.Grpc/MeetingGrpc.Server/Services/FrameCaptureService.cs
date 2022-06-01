3f5416cd64550d8564364b4c635bf6f184709b6c
limeniye <limeniye@gmail.com>
2022-06-01 20:12:06 +0300

    - Created methods for FrameCaptureService;

--- src/MeetingServer.Grpc/MeetingGrpc.Server/Services/FrameCaptureService.cs
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Services/FrameCaptureService.cs
@@ -46,5 +46,107 @@ namespace MeetingGrpc.Server.Services
                 _logger.LogError($"{peer} frame captures unsubscribed.");
             }
         }
+
+        [Authorize]
+        public override Task<Empty> SendFrameCapture(FrameCaptureRequest request, ServerCallContext context)
+        {
+            _logger.LogInformation($"{context.Peer} {request}");
+
+            //if (!Guid.TryParse(request., out Guid messageGuid))
+            //    throw new NotImplementedException();
+
+            var user = context.GetHttpContext().User;
+
+            throw new NotImplementedException(" Should to finde user from context");
+
+            //_chatService.Add(new Message(messageGuid, request.Message, Timestamp.FromDateTime(DateTime.UtcNow),
+            //                    new User()
+            //    Username = username,
+            //    MessageGuid = request.MessageGuid,
+            //    Message = request.Message,
+            //    Time = ,
+            //    // TODO : Temporary
+            //    UserGuid = request.UserGuid
+            //});
+
+            return Task.FromResult(empty);
+        }
+
+        [AllowAnonymous]
+        public override async Task FrameCaptureStatesSubscribe(Empty request, IServerStreamWriter<FrameCaptureState> responseStream, ServerCallContext context)
+        {
+            var peer = context.Peer;
+            _logger.LogInformation($"{peer} frame captures subscribes.");
+
+            context.CancellationToken.Register(() => _logger.LogInformation($"{peer} cancel frame captures subscription."));
+
+            try
+            {
+                await _frameCapturesService.CaptureFrameStatesAsObservable()
+                    .ToAsyncEnumerable()
+                    .ForEachAwaitAsync(async (x) => await responseStream.WriteAsync(
+                        new FrameCaptureState
+                        {
+                            UserGuid = x.UserGuid,
+                            IsOn = x.IsOn
+                        }), context.CancellationToken)
+                    .ConfigureAwait(false);
+            }
+            catch (TaskCanceledException)
+            {
+                _logger.LogError($"{peer} frame captures unsubscribed.");
+            }
+        }
+
+        [Authorize]
+        public override Task<Empty> TurnOffFrameCapture(Empty request, ServerCallContext context)
+        {
+            _logger.LogInformation($"{context.Peer} {request}");
+
+            //if (!Guid.TryParse(request., out Guid messageGuid))
+            //    throw new NotImplementedException();
+
+            var user = context.GetHttpContext().User;
+
+            throw new NotImplementedException(" Should to finde user from context");
+
+            //_chatService.Add(new Message(messageGuid, request.Message, Timestamp.FromDateTime(DateTime.UtcNow),
+            //                    new User()
+            //    Username = username,
+            //    MessageGuid = request.MessageGuid,
+            //    Message = request.Message,
+            //    Time = ,
+            //    // TODO : Temporary
+            //    UserGuid = request.UserGuid
+            //});
+
+            return Task.FromResult(empty);
+        }
+
+
+        [Authorize]
+        public override Task<Empty> TurnOnFrameCapture(Empty request, ServerCallContext context)
+        {
+            _logger.LogInformation($"{context.Peer} {request}");
+
+            //if (!Guid.TryParse(request., out Guid messageGuid))
+            //    throw new NotImplementedException();
+
+            var user = context.GetHttpContext().User;
+
+            throw new NotImplementedException(" Should to finde user from context");
+
+            //_chatService.Add(new Message(messageGuid, request.Message, Timestamp.FromDateTime(DateTime.UtcNow),
+            //                    new User()
+            //    Username = username,
+            //    MessageGuid = request.MessageGuid,
+            //    Message = request.Message,
+            //    Time = ,
+            //    // TODO : Temporary
+            //    UserGuid = request.UserGuid
+            //});
+
+            return Task.FromResult(empty);
+        }
     }
 }
