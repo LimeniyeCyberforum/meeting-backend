9331dcbe53eb1eee685a17d14e9ccbd4b31e780e
limeniye <limeniye@gmail.com>
2022-06-01 04:24:50 +0300

    - Added DateTime to Message;

--- src/MeetingServer.Grpc/MeetingGrpc.Server/Model/Message.cs
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Model/Message.cs
@@ -3,13 +3,18 @@
     public class Message
     {
         public Guid Guid { get; }
+
         public string Content { get; }
+
+        public DateTime DateTime { get; }
+
         public User User { get; }
 
-        public Message(Guid guid, string content, User user)
+        public Message(Guid guid, string content, DateTime dateTime, User user)
         {
             Guid = guid;
             Content = content;
+            DateTime = dateTime;
             User = user;
         }
     }
