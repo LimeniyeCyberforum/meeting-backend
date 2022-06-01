b861dc91a9bb115690334ee69b6c2ee054c883d6
limeniye <limeniye@gmail.com>
2022-06-01 21:12:21 +0300

    - Added new dependency types;

--- src/MeetingServer.Grpc/MeetingGrpc.Server/Program.cs
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Program.cs
@@ -1,19 +1,30 @@
-using GrpsServer.Model;
-using GrpsServer.Persistence;
-using GrpsServer.Services;
+using MeetingGrpc.Protos;
+using MeetingGrpc.Repositories.LocalServices;
+using MeetingGrpc.Server.Model;
+using MeetingGrpc.Server.Repositories;
+using MeetingGrpc.Server.Repositories.LocalServices;
+using MeetingGrpc.Server.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
-builder.Services.AddSingleton<IChatLogRepository, ChatRepository>();
-builder.Services.AddSingleton<IUsersCameraCaptureRepository, UsersCameraCaptureRepository>();
-builder.Services.AddSingleton<ChatService>();
-builder.Services.AddSingleton<UsersCameraCaptureService>();
+
+builder.Services.AddSingleton<IRepository<Message>, ChatRepository>();
+builder.Services.AddSingleton<IRepository<FrameCaptureState>, FrameCaptureStatesRepository>();
+builder.Services.AddSingleton<IRepository<User>, UsersRepository>();
+
+builder.Services.AddSingleton<LocalUsersService>();
+builder.Services.AddSingleton<LocalChatService>();
+builder.Services.AddSingleton<LocalFrameCapturesService>();
 
 var app = builder.Build();
 
 app.UseGrpcWeb();
-app.MapGrpcService<MeetingService>().EnableGrpcWeb()/*.RequireHost("*:5010")*/;
+app.MapGrpcService<AuthorizationService>().EnableGrpcWeb();
+app.MapGrpcService<ChatService>().EnableGrpcWeb();
+app.MapGrpcService<FrameCaptureService>().EnableGrpcWeb();
+app.MapGrpcService<UsersService>().EnableGrpcWeb();
+
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
