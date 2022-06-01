66e8a6e74dabc80c447e5b8d647a8de4d5b4d307
limeniye <limeniye@gmail.com>
2022-06-01 21:07:14 +0300

    - Added UsersRepository;

---
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Repositories/UsersRepository.cs
@@ -0,0 +1,24 @@
+﻿using MeetingGrpc.Server.Model;
+
+namespace MeetingGrpc.Server.Repositories
+{
+    public class UsersRepository : IRepository<User>
+    {
+        private readonly List<User> localStorage = new List<User>(); // dummy on memory storage
+
+        public void Add(User user)
+        {
+            localStorage.Add(user);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return localStorage.AsReadOnly();
+        }
+
+        public void Remove(User obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
