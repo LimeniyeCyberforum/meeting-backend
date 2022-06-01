5d7883b16981649f23c93b0163752ed301f3bb13
limeniye <limeniye@gmail.com>
2022-06-01 20:17:30 +0300

    - ChatRepository completed;

---
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Repositories/ChatRepository.cs
@@ -0,0 +1,26 @@
+﻿using MeetingGrpc.Protos;
+using MeetingGrpc.Server.Model;
+using System.ComponentModel.Composition;
+
+namespace MeetingGrpc.Server.Repositories
+{
+    public class ChatRepository : IRepository<Message>
+    {
+        private readonly List<Message> localStorage = new List<Message>(); // dummy on memory storage
+
+        public void Add(Message message)
+        {
+            localStorage.Add(message);
+        }
+
+        public IEnumerable<Message> GetAll()
+        {
+            return localStorage.AsReadOnly();
+        }
+
+        public void Remove(Message obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
