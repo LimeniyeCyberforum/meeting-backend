e5e11add97e617dc2055703e4df415480c9a0100
limeniye <limeniye@gmail.com>
2022-06-05 01:22:06 +0300

    - Added EventAction and ActionHelper;

---
+++ src/MeetingServer.Grpc/MeetingGrpc.Server/Repositories/LocalServices/EventAction.cs
@@ -0,0 +1,40 @@
+﻿namespace MeetingGrpc.Server.Repositories.LocalServices
+{
+    public enum EventAction
+    {
+        Added,
+        Removed,
+        Changed
+    }
+
+    public static class ActionHelper
+    {
+        public static Protos.Action ToProtosAction(this EventAction action)
+        {
+            switch (action)
+            {   
+                case EventAction.Added:
+                    return Protos.Action.Added;
+                case EventAction.Removed:
+                    return Protos.Action.Removed;
+                case EventAction.Changed:
+                    return Protos.Action.Changed;
+            }
+            throw new NotImplementedException();
+        }
+
+        public static EventAction ToEventAction(this Protos.Action action)
+        {
+            switch (action)
+            {
+                case Protos.Action.Added:
+                    return EventAction.Added;
+                case Protos.Action.Removed:
+                    return EventAction.Removed;
+                case Protos.Action.Changed:
+                    return EventAction.Changed;
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
